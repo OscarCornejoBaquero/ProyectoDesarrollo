b627f49b8ebfc9a87ffecbc873ffd4369a322ed7
Oscar Cornejo <oscar.cornejob@ug.edu.ec>
2021-09-13 22:06:59 -0500

    Inicio del Proyecto

---
+++ Views/home.php
@@ -0,0 +1,18 @@
+<?php
+
+echo ("Prueba lista");
+echo (base_url());
+print_r($data);
+
+?>
+<br>
+<br>
+<?php
+print_r(dep($data));
+
+echo (passGenerador());
+echo ("<br>");
+echo (token());
+echo ("<br>");
+echo (SMONEY.formatMoney(22222));
+?>
